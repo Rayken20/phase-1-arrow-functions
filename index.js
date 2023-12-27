d291badf2bda6447bbbd746c52adc9792af9217d
Rachael <rachael92njoki@gmail.com>
2023-12-27 09:37:14 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide= (num1, num2)=> {num1/num2;
+return 20
+}
+divide(2000, 100);
+
+const square=parameter1=>{parameter1*parameter1;
+    return 4
+}
+square();
+
+
+const add=(par1, par2)=>{
+    return par1+par2
+
+}
+add ();
